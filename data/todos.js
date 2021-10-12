299d26dba6ead4ed10af470a9ad9c76c41db6a47
Frank Hollywood <github@srvsync.com>
2021-10-11 17:26:57 -0700

    built app

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    todo: 'wash dishes',
+    completed: false,
+    
+  },
+  {
+    todo: 'read for school',
+    completed: false,
+    
+  },
+  {
+    todo: 'practice coding',
+    completed: false,
+    
+  }
+];
